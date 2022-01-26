276cd84c2c2487d7fef54e2041091aaf22c4c6cb
Krb <maurodavidcaceres@gmail.com>
2022-01-26 15:45:00 -0300

    main upload

---
+++ components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+const index = () => {
+  return (
+    <Container>
+      <h2>Hey</h2>
+    </Container>
+  );
+};
+
+export default index;
+
+const Container = styled.div``;
+
+const Header = styled.div``;
