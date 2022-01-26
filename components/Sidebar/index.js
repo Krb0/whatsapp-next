6974f72f548bf121d0101ec5b5fd3627f66e00a0
Krb <maurodavidcaceres@gmail.com>
2022-01-26 18:54:28 -0300

    Sidebar added

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,9 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import * as EmailValidator from "email-validator";
 const index = () => {
+  const createChat = () => {
+    const input = prompt(
+      "Please enter an email address for the user you wish to chat with"
+    );
+    if (!input) return;
+    if (EmailValidator.validate(input)) {
+    }
+  };
+
   return (
     <Container>
-      <h2>Hey</h2>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+      <SidebarButton>Start a new chat</SidebarButton>
     </Container>
   );
 };
@@ -12,4 +43,45 @@ export default index;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+const SearchInput = styled.input`
+  border: none;
+  outline-width: 0;
+  flex: 1;
+`;
+const SidebarButton = styled(Button)`
+  width: 100%;
+  font-weight: 600;
+  color: #242424;
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.85;
+  }
+`;
+
+const IconsContainer = styled.div``;
